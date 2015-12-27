8663647916dbf7813ee1388c01edfcb30cc987d5
Atheed Thameem <atheedthameem@hotmail.com>
2015-12-26 21:14:35 -0600

    implemented onclick listeners and message passing for the background and content scripts. updated manifest.json appropriately

---
+++ extension/src/content.js
@@ -0,0 +1,25 @@
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if( request.message === "clicked_browser_action" ) {
+            // sample jQuery.ajax() call to get related articles for a particular stirng
+            $.ajax({
+                type: "POST",
+                url: "https://news-api.lateral.io/recommend-by-text/",
+                headers: { 
+                    "subscription-key": "21b109cfad5536905f5cf081ed599cee"
+                },
+                dataType: "json",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    "text":"Machine learning is a subfield of computer science that evolved from the study of pattern recognition and computational learning theory in artificial intelligence. Machine learning explores the construction and study of algorithms that can learn from and make predictions on data."
+                }),
+                success: function (data) {
+                    console.log(JSON.stringify(data));
+                },
+                error: function () {
+                    console.log("Error");
+                }
+            });
+        }
+    }
+);
