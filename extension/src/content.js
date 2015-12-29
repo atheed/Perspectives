8db003de54f4f0af4104cecbe02d6cc0f5fe82f1
Atheed Thameem <atheedthameem@hotmail.com>
2015-12-28 19:24:02 -0600

    changed content.js to get important keywords from current page, and pass those to Lateral API, which finds relevant articles for those keywords

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,5 +1,34 @@
 var textResponse;
 var jsonResponse;
+var uniqueListOfWords;
+var currentUrl = window.location.href;
+
+// sample jQuery.ajax() call to get important keywords in a text
+$.ajax({
+    type: "POST",
+    url: "https://gateway-a.watsonplatform.net/calls/url/URLGetRankedKeywords",
+    async: false,
+    data: {
+        "url":currentUrl,
+        "apikey":"70feaf9092b12babf41a9a9909499c1307091db0",
+        "maxRetrieve":"10",
+        "keywordExtractMode":"strict",
+        "outputMode":"json"
+    },
+    success: function (data) {
+        textResponse = JSON.stringify(data);
+        var obj = $.parseJSON(textResponse);
+        var keywords = "";
+        $.each(obj.keywords, function(i, keyword) {
+            keywords = keywords + keyword.text + " ";
+        });
+        uniqueListOfWords = getUniqueKeywords(keywords);
+        console.log(uniqueListOfWords);
+    },
+    error: function () {
+        textResponse = "Error";
+    }
+});
 
 // sample jQuery.ajax() call to get related articles for a particular stirng
 $.ajax({
@@ -8,10 +37,11 @@ $.ajax({
     headers: { 
         "subscription-key": "21b109cfad5536905f5cf081ed599cee"
     },
+    async: false,
     dataType: "json",
     contentType: "application/json",
     data: JSON.stringify({
-        "text":"Machine learning is a subfield of computer science that evolved from the study of pattern recognition and computational learning theory in artificial intelligence. Machine learning explores the construction and study of algorithms that can learn from and make predictions on data."
+        "text":uniqueListOfWords
     }),
     success: function (data) {
         jsonResponse = JSON.stringify(data);
@@ -21,32 +51,19 @@ $.ajax({
     }
 });
 
-// sample jQuery.ajax() call to get important keywords in a text
-$.ajax({
-    type: "POST",
-    url: "https://gateway-a.watsonplatform.net/calls/text/TextGetRankedKeywords",
-    data: {
-        "text":"US Vice-President Joe Biden has urged Japan and South Korea to improve their relations and co-operation. Mr Biden was in Seoul on the third leg of an Asian tour dominated by tensions over China newly-declared air defence zone.",
-        "apikey":"70feaf9092b12babf41a9a9909499c1307091db0",
-        "maxRetrieve":"10",
-        "keywordExtractMode":"strict",
-        "outputMode":"json"
-    },
-    success: function (data) {
-        textResponse = JSON.stringify(data);
-    },
-    error: function () {
-        textResponse = "Error";
-    }
-});
+function getUniqueKeywords(keywords) {
+    return keywords.split(' ').filter(function(allItems,i,a){
+        return i==a.indexOf(allItems);
+    }).join(' ');
+};
 
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message === "clicked_browser_action") {
+            //console.log(textResponse);
+            //console.log("-----------");
             console.log(jsonResponse);
-            console.log("-----------");
-            console.log(textResponse);
         }
     }
 );
