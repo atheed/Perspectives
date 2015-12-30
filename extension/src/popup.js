ea6cfadc978eb7f13e4051a95fe1e5956a4c5c7b
Atheed Thameem <atheedthameem@hotmail.com>
2015-12-30 15:25:34 -0600

    indentation-spacing fix in popup.js

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -3,15 +3,15 @@
 chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"}, function(relatedArticles) {
-    	$("#loadingContent").html("");
-    	$("#relatedArticles").html("");
-    	if (relatedArticles === "Error") {
-    		$("#relatedArticles").append("There is not enough context on this page to find related articles.");
-    	} else {
-    		$.each(relatedArticles, function(i, article) {
-	    		$("#relatedArticles").append("<div class='article'><a class='article-link' target='_blank' href='"  + article[1] + "'>" + article[0] + "</a><br />" + 
-	    			"<h5>Published on: " + article[2] + "<br />Source: " + article[3] + "</h5>" + "<p></p>" + "</div>");
-	    	});
-    	}
+        $("#loadingContent").html("");
+        $("#relatedArticles").html("");
+        if (relatedArticles === "Error") {
+            $("#relatedArticles").append("There is not enough context on this page to find related articles.");
+        } else {
+            $.each(relatedArticles, function(i, article) {
+                $("#relatedArticles").append("<div class='article'><a class='article-link' target='_blank' href='"  + article[1] + "'>" + article[0] + "</a><br />" + 
+                    "<h5>Published on: " + article[2] + "<br />Source: " + article[3] + "</h5>" + "<p></p>" + "</div>");
+            });
+        }
     });
 });
