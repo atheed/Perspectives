f2e4a452d4bc1ff98d3834bf15ccdd8429b0e0ef
Atheed Thameem <atheedthameem@hotmail.com>
2015-12-30 15:14:36 -0600

    fixed popup links issue, and moved css to its own file (close #10, close #8)

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -6,21 +6,12 @@ chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     	$("#loadingContent").html("");
     	$("#relatedArticles").html("");
     	if (relatedArticles === "Error") {
-    		$("#relatedArticles").append("There is not enough context on this page to find related articles");
+    		$("#relatedArticles").append("There is not enough context on this page to find related articles.");
     	} else {
     		$.each(relatedArticles, function(i, article) {
-	    		$("#relatedArticles").append("<div class='article'><a class='article-link' href='"  + article[1] + "'>" + article[0] + "</a><br />" + 
+	    		$("#relatedArticles").append("<div class='article'><a class='article-link' target='_blank' href='"  + article[1] + "'>" + article[0] + "</a><br />" + 
 	    			"<h5>Published on: " + article[2] + "<br />Source: " + article[3] + "</h5>" + "<p></p>" + "</div>");
 	    	});
     	}
     });
 });
-
-
-
-/*
-
-if (relatedArticles === "Error") {
-    		$("#relatedArticles").append("There is not enough context on this page to find related articles");
-    	}
-*/
